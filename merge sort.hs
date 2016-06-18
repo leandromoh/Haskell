2486de69c12a0aee719ad05c491aad0a90d4a899
leandromoh <leandromoh@hotmail.com>
2016-06-18 11:08:08 -0300

    adjust on merge sort

--- merge sort.hs
+++ merge sort.hs
@@ -9,9 +9,9 @@ intercala (x:xs) (y:ys)
   | x <= y = x:intercala xs (y:ys)
   | otherwise = y:intercala (x:xs) ys
 
-ordena [] = []
-ordena [x] = [x]
-ordena xs = intercala (ordena (rec !! 0)) (ordena (rec !! 1))
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs = intercala (mergeSort (rec !! 0)) (mergeSort (rec !! 1))
             where rec = distribui xs
 
-main = print $ ordena [0,9,2,7,4,5,6,3,8,1]
+main = print $ mergeSort [0,9,2,7,4,5,6,3,8,1]
