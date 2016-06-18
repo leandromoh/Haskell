72bdfec68015d536784d0757c7a6ee5d65681b8e
leandromoh <leandromoh@hotmail.com>
2016-06-18 11:22:06 -0300

    changes

--- programas/indexOf.hs
+++ programas/indexOf.hs
@@ -1,16 +1,16 @@
 indexOf str sub startPosition
   | startPosition >= len1 = -1
   | len1 < len2 = -1
-  | otherwise = goThroughString str sub startPosition (len1 - len2)
+  | otherwise = goThroughList str sub startPosition (len1 - len2)
   where
     len1 = length str
     len2 = length sub
     
-goThroughString [] _ _ _ = -1
-goThroughString (x:xs) sub pos maxInd
+goThroughList [] _ _ _ = -1
+goThroughList (x:xs) sub pos maxInd
   | pos > maxInd = -1
   | tryMatch (x:xs) sub = pos
-  | otherwise = goThroughString xs sub (pos+1) maxInd
+  | otherwise = goThroughList xs sub (pos+1) maxInd
 
 tryMatch _ [] = True
 tryMatch [] (y:ys) = False
