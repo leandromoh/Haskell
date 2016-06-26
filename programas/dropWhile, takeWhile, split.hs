a90be3053cedeb90456dee7fa4510e943fdaf5a1
Leandro Fernandes <leandromoh@hotmail.com>
2016-06-25 22:17:13 -0300

    Update dropWhile, takeWhile, split.hs

--- programas/dropWhile, takeWhile, split.hs
+++ programas/dropWhile, takeWhile, split.hs
@@ -1,8 +1,11 @@
 dropWhile' f [] = []
-dropWhile' f (x:xs) = if f x then dropWhile' f xs else xs
+dropWhile' f (x:xs) = if f x then dropWhile' f xs else x:xs
 
 takeWhile' f [] = []
 takeWhile' f (x:xs) = if f x then x : takeWhile' f xs else []
 
-split c [] = []
-split c xs = (takeWhile' (/= c) xs) : split c (dropWhile' (/= c) xs)
+split p [] = []
+split p xs = (takeWhile' px xs) : split p (drop 1 (dropWhile' px xs))
+  where px = not . p
+
+main = print $ split (==3) [1,2,3,4,5]
