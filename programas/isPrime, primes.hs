2b1d9722c6afd6da118e5adb95b4881af22ce490
Leandro Fernandes <leandromoh@hotmail.com>
2016-10-07 01:38:02 -0300

    Update isPrime, primes.hs

--- programas/isPrime, primes.hs
+++ programas/isPrime, primes.hs
@@ -3,6 +3,8 @@ checkprime (x:xs) n
   | mod n x == 0 = False
   | otherwise = checkprime xs n
 
+isPrime 1 = False
+isPrime 2 = True
 isPrime n = checkprime [2..(n - 1)] n
 
 primes = [ p | p <- [1..] , isPrime p]
