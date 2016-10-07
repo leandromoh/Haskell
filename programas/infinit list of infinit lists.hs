e1a6f6e67a74572aa78a17ae485de1a2f744c4c4
Leandro Fernandes <leandromoh@hotmail.com>
2016-10-07 00:44:21 -0300

    infinit list of infinit lists

---
+++ programas/infinit list of infinit lists.hs
@@ -0,0 +1,14 @@
+checkprime [] n = True 
+checkprime (x:xs) n 
+  | mod n x == 0 = False
+  | otherwise = checkprime xs n
+
+isPrime n = checkprime [2..(n - 1)] n
+
+primes = [ p | p <- [1..] , isPrime p]
+
+powList x = map (x^) [1..]
+
+infinits = map powList primes
+
+main = print $  map (take 3) (take 10 infinits)
