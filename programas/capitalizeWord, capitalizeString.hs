fc81b43dbf3a7a0bf0d6ab0c7dd947eb41dc5f51
Leandro Fernandes <leandromoh@hotmail.com>
2016-12-23 19:33:07 -0200

    Update capitalizeWord, capitalizeString.hs

--- programas/capitalizeWord, capitalizeString.hs
+++ programas/capitalizeWord, capitalizeString.hs
@@ -1,28 +1,13 @@
 import Data.Char
+import Data.List
 
-dropWhile' f [] = []
-dropWhile' f (x:xs) = if f x then dropWhile' f xs else xs
-
-takeWhile' f [] = []
-takeWhile' f (x:xs) = if f x then x : takeWhile' f xs else []
-
-split c [] = []
-split c xs = (takeWhile' (/= c) xs) : split c (dropWhile' (/= c) xs)
-
-join c [] = []
-join c (x:[]) = x
-join c (x:xs) = x ++ c ++ (join c xs)
-
-isRoman [] = True
-isRoman (x:xs)
-  | not (elem x ['I','V','X','L','C','D','M']) = False
-  | otherwise = isRoman xs
+isRoman = all (flip elem ['I','V','X','L','C','D','M'])
 
 capitalizeWord str@(x:xs)
   | isRoman str = map toUpper str
   | length str > 3 = toUpper x : map toLower xs
   | otherwise = map toLower str
 
-capitalizeString xs = (join " " (map capitalizeWord (split ' ' xs)))
+capitalizeString = intercalate " " . map capitalizeWord . words
 
 main = print $ map capitalizeString ["ALGORÍTIMOS E LÓGICA DE PROGRAMAÇÃO", "LINGUAGEM DE PROGRAMAÇÃO", "LINGUAGEM DE PROGRAMAÇÃO IV", "LABORATÓRIO DE BANCO DE DADOS", "ESTRUTURAS DE DADOS", "ENGENHARIA DE SOFTWARE III", "PROGRAMAÇÃO ORIENTADA A OBJETOS"]
