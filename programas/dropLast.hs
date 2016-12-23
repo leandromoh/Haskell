1fc6738cd40bf25eede7e7ffe78191a357ff0e67
Leandro Fernandes <leandromoh@hotmail.com>
2016-12-23 20:54:15 -0200

    Update dropLast.hs

--- programas/dropLast.hs
+++ programas/dropLast.hs
@@ -1,17 +1,8 @@
---ghc 8.0.1 /opt/ghc/8.0.1/lib/ghc-8.0.0.20160127/
+dropLast n xs 
+ | n < 1 = xs 
+ | otherwise = let s = splitAt n xs in bla (fst s) (snd s)
+ where 
+  bla _ [] = []
+  bla (x:xs) (y:ys) = x : bla (xs ++ [y]) ys
 
-dropLast xs 0 = xs
-dropLast xs n
- | z == [] = []
- | otherwise = dae (z !! 0) (z !! 1) []
- where z = bla xs [] n
-
-bla [] _ _ = []
-bla (x:xs) ys n
- | n > 0 = bla xs (ys ++ [x]) (n-1)
- | otherwise = [(x:xs), ys]
-
-dae [] _ zs = zs
-dae (x:xs) (y:ys) zs = dae xs (ys ++ [x]) (zs ++ [y])
-
-main = print $ dropLast [1..5] 2
+main = print $ take 3 $ dropLast 100 [1..]
