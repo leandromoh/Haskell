608a4f1c32f47d993644eea0a45d565f190f5346
Leandro Fernandes <leandromoh@hotmail.com>
2016-12-24 11:36:00 -0200

    Update map, filter, fold.hs

--- programas/map, filter, fold.hs
+++ programas/map, filter, fold.hs
@@ -5,3 +5,14 @@ filter' p =  foldr (\x acc -> if p x then x : acc else acc) []
 foldl' f a bs = foldr (\b g x -> g (f x b)) id bs a
 
 main = print $ foldl' (+) 0 [1..3]
+
+
+
+
+--- foldr (+) 0 [1,2,3] = + 1 (+ 2 (+ 3 0))
+foldr' f init [] = init
+foldr' f init (x:xs) = f x (foldr' f init xs)
+    
+--- foldl (^) 2 [3,4] = (^ (^ 2 3) 4) 
+foldl' f init [] = init                   
+foldl' f init (x:xs) = foldl' f (f init x) xs
